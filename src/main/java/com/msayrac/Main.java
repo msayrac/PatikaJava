0c3db625f08956f419e9d05cd93b8f664ccc83af
msayrac <msayracode@gmail.com>
2025-03-15 10:10:59 +0300

    Note Calculator

---
+++ src/main/java/com/msayrac/Main.java
@@ -0,0 +1,33 @@
+package com.msayrac;
+
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        // variables
+        int mat, fizik, kimya, turkce, tarih, muzik;
+
+        //kullanıcıdan deger al
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Matematik Notunu Giriniz : ");
+        mat = input.nextInt();
+        System.out.println("Fizik Notunu Giriniz : ");
+        fizik = input.nextInt();
+        System.out.println("Kimya Notunu Giriniz : ");
+        kimya = input.nextInt();
+        System.out.println("Türkçe Notunu Giriniz : ");
+        turkce = input.nextInt();
+        System.out.println("Müzik Notunu Giriniz : ");
+        muzik = input.nextInt();
+
+        int toplam = mat + fizik + kimya + turkce + muzik;
+        double sonuc = toplam / 6;
+        System.out.println("Ortalamanız : " + sonuc);
+
+
+    }
+}
