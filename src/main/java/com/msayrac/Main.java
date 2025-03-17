b62883283dc66b8e283d254e26cb07bb2a518c19
msayrac <msayracode@gmail.com>
2025-03-17 09:27:21 +0300

    Methods covered

--- src/main/java/com/msayrac/Main.java
+++ src/main/java/com/msayrac/Main.java
@@ -1,26 +1,24 @@
 package com.msayrac;
 
+
 import java.util.Scanner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+
+    static int power(int base, int exp) {
+
+        return (int) Math.pow(base,exp);
+
+
+
+
+    }
+
     public static void main(String[] args) {
 
-        // variables
-        double kdv = 0.18;
-        double miktar;
-        double kdvMiktar;
 
-        Scanner input = new Scanner(System.in);
-
-        System.out.println("KDV hesaplanacak Tutarı Giriniz :");
-        miktar = input.nextDouble();
-
-        System.out.println("Girilen Paranız : " + miktar);
-        kdvMiktar = miktar * kdv + miktar;
-        System.out.println("Kdv Hesaplanan Paranız : " + kdvMiktar);
-        System.out.println("KDV Tutar : " +miktar*kdv);
+        int case1 =power(2,3);
+        System.out.println(case1);
 
 
     }
