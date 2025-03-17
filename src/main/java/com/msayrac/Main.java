6b41cb6500ede828046f62973d39bf22f5ddaa28
msayrac <msayracode@gmail.com>
2025-03-17 11:46:05 +0300

    Access modifier is covered

--- src/main/java/com/msayrac/Main.java
+++ src/main/java/com/msayrac/Main.java
@@ -1,25 +1,29 @@
 package com.msayrac;
 
-
 import java.util.Scanner;
 
 public class Main {
 
-    static int power(int base, int exp) {
-
-        return (int) Math.pow(base,exp);
-
-
-
-
-    }
 
     public static void main(String[] args) {
 
+        Teacher t1 = new Teacher("Mahmut", "TRH", "555");
+        Teacher t2 = new Teacher("Graham", "FZK", "444");
+        Teacher t3 = new Teacher("Kulyutmaz", "BIO", "111");
 
-        int case1 =power(2,3);
-        System.out.println(case1);
+        Course tarih = new Course("Tarih", "101", "TRH");
+        tarih.addTeacher(t1);
 
+        Course fizik = new Course("Fizik", "102", "FZK");
+        fizik.addTeacher(t2);
+
+        Course biyo = new Course("Biyoloji", "103", "BIO");
+        biyo.addTeacher(t3);
+
+
+        Student s1 = new Student("Saban", "123", "4", tarih, fizik, biyo);
+        s1.addBulkExamNote(0, 0, 0);
+        s1.isPass();
 
     }
 }
